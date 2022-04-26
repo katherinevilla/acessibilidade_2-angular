b93feb9d319d3402ee96e0c5ffd386185c94f49a
katherine villaparedes <86531447+katherinevilla@users.noreply.github.com>
2022-04-26 10:05:53 -0400

    adicionando modal

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { fade } from './shared/animations/fade';
+import { ModalRef } from './shared/components/modal/models/modal-ref';
+import {  ModalService } from './shared/components/modal/services/modal.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [fade],
+})
+export class AppComponent {
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
+  title = 'a11y-p2';
+  public firstName = 'kathe';
+  public modalRef: ModalRef;
+  public info = false;
+
+  constructor(private modalService: ModalService) {}
+
+  public show(): void {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details'
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ModalModule } from './shared/components/modal/modal.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ModalModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
